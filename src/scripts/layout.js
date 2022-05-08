b9276cfe4794c00260f448f75ad5c93d239fa7a9
AMS2302 <ams23post@gmail.com>
2022-05-09 01:52:59 +0300

    style: add styles to layout

--- src/scripts/layout.js
+++ src/scripts/layout.js
@@ -10,9 +10,24 @@ export function showLayout() {
     const element = createTag(item, [item]);
     const container = createTag('div', ['container', `container_${item}`]);
     element.appendChild(container);
+    if (item === 'header') {
+      const title = createTag('h1', ['header__title']);
+      title.innerHTML = 'RSS Virtual Keyboard';
+      container.appendChild(title);
+    }
     if (item === 'main') {
+      const textarea = createTag('textarea', ['textarea']);
+      container.appendChild(textarea);
       const keyboard = createKeyboard();
       container.appendChild(keyboard);
+    }
+    if (item === 'footer') {
+      const content = createTag('p', ['footer__content']);
+      content.innerHTML = `
+      Клавиатура создана в операционной системе Windows.<br>
+      Для переключения языка: Alt + Shift.
+      `;
+      container.appendChild(content)
     }
     body.appendChild(element);
   })
