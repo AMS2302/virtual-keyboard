7e1a6af080ac70e5785addd98fae83da80e02590
AMS2302 <ams23post@gmail.com>
2022-05-08 12:54:59 +0300

    feat: add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/scripts/index.js',
+  output: {
+    filename: '[hash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    port: 3002,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack-Config',
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new ESLintPlugin({extensions: 'js'}),
+  ],
+};
