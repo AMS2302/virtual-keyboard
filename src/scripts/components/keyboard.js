3b1b33eb5a25879411563694fe92e5e681fecf7c
AMS2302 <ams23post@gmail.com>
2022-05-09 23:23:30 +0300

    style: add animation to buttons

--- src/scripts/components/keyboard.js
+++ src/scripts/components/keyboard.js
@@ -12,17 +12,15 @@ export function createKeyboard() {
 function handleEvent(event) {
   const button = document.getElementById(event.code);
   const textarea = document.querySelector('.textarea');
-  if (button) {
-    textarea.focus();
-    if (event.key === 'Alt' || event.key === 'Control') {
-      event.preventDefault();
-      textarea.blur();
-    }
-    button.classList.add('button_active');
-    document.addEventListener('keyup', () => {
-      button.classList.remove('button_active');
-    });
+  textarea.focus();
+  if (event.key === 'Alt' || event.key === 'Control') {
+    event.preventDefault();
+    textarea.blur();
   }
+  button.classList.add('button_active');
+  document.addEventListener('keyup', () => {
+    button.classList.remove('button_active');
+  });
 }
 
 export function handleKeyboardEvent() {
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,8 +5,36 @@ import { config } from './config.js';
 
 import { showLayout } from './layout.js';
 import { showButtons } from './components/buttons.js';
-import {handleKeyboardEvent} from './components/keyboard.js'
+import { handleKeyboardEvent } from './components/keyboard.js'
 
 showLayout();
 showButtons(config);
 handleKeyboardEvent();
+
+
+// ==================== try new functionality ==========================
+
+// const button = document.getElementById('KeyG');
+
+// button.addEventListener('mousedown', function (event) {
+//   event.preventDefault();
+//   document.dispatchEvent(
+//     new KeyboardEvent("keydown", {
+//       code: "KeyG",
+//       key: "KeyG",
+//       bubbles: true,
+//       // composed: true,
+//     })
+//   );
+// });
+
+// button.addEventListener('mouseup', function (event) {
+//   event.preventDefault();
+//   document.dispatchEvent(
+//     new KeyboardEvent("keyup", {
+//       code: "KeyG",
+//       key: "KeyG",
+//       bubbles: true,
+//     })
+//   );
+// });
